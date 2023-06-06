63e114961159427b0d978c313d1f05a404ad87e1
piscinagua <correo_javi@hotmail.com>
2023-06-06 19:30:06 +0200

    Mejora la validación con campos requeridos de forma nativa.

--- Actividad1/src/Parejas.tsx
+++ Actividad1/src/Parejas.tsx
@@ -3,16 +3,16 @@ import { IMiembro } from "./interfaces/miembro.interface";
 import { IGente } from "./interfaces/gente.interface";
 
 export const Parejas = () => {
-	const [miembro1, setMiembro1] = useState<IMiembro>({miembro: ""});
-	const [miembro2, setMiembro2] = useState<IMiembro>({miembro: ""});
+	const [miembro1, setMiembro1] = useState<IMiembro>({ miembro: "" });
+	const [miembro2, setMiembro2] = useState<IMiembro>({ miembro: "" });
 	const grupo1: IGente[] = [];
 
 	const onChangeMiembro1 = (e: ChangeEvent<HTMLInputElement>) => {
-		setMiembro1({miembro: e.target.value});
+		setMiembro1({ miembro: e.target.value });
 	};
 
 	const onChangeMiembro2 = (e: ChangeEvent<HTMLInputElement>) => {
-		setMiembro2({miembro: e.target.value});
+		setMiembro2({ miembro: e.target.value });
 	};
 
 	const onSubmit = (e: FormEvent<HTMLFormElement>) => {
@@ -25,14 +25,10 @@ export const Parejas = () => {
 			<hr />
 			<form onSubmit={onSubmit}>
 				<label htmlFor="miembro1">Miembro grupo 1:</label>
-				<input id="miembro1" type="text" onChange={onChangeMiembro1} />
+				<input id="miembro1" type="text" onChange={onChangeMiembro1} required />
 				<label htmlFor="miembro2">Miembro grupo 2:</label>
-				<input id="miembro2" type="text" onChange={onChangeMiembro1} />
-				<button
-					className="btn btn-success"
-					type="submit"
-					disabled={miembro1.miembro.trim() === "" && miembro2.miembro.trim() === ""}
-				>
+				<input id="miembro2" type="text" onChange={onChangeMiembro1} required />
+				<button className="btn btn-success" type="submit">
 					Añadir
 				</button>
 			</form>
