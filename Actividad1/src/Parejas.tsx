a4198a8d1c10ec8664c99ef3853ac7ec213e9086
piscinagua <correo_javi@hotmail.com>
2023-06-06 19:03:09 +0200

    Gracias a JL, corrige los errores al usar tipos complejos en lugar de tipos básicos para el formulario.
    Co-authored-by: juanluisochoa@hotmail.com <juanluisochoa@hotmail.com>

--- Actividad1/src/Parejas.tsx
+++ Actividad1/src/Parejas.tsx
@@ -3,41 +3,38 @@ import { IMiembro } from "./interfaces/miembro.interface";
 import { IGente } from "./interfaces/gente.interface";
 
 export const Parejas = () => {
-	const [miembro1, setMiembro1] = useState<IMiembro>();
-	const [miembro2, setMiembro2] = useState<IMiembro>();
-	const grupo1: IGente = [];
-
+	const [miembro1, setMiembro1] = useState<IMiembro>({miembro: ""});
+	const [miembro2, setMiembro2] = useState<IMiembro>({miembro: ""});
+	const grupo1: IGente[] = [];
 
 	const onChangeMiembro1 = (e: ChangeEvent<HTMLInputElement>) => {
-		setMiembro1(e.target.value);
-	  };
+		setMiembro1({miembro: e.target.value});
+	};
 
-	  const onChangeMiembro2 = (e: ChangeEvent<HTMLInputElement>) => {
-		setMiembro2(e.target.value);
-	  };
+	const onChangeMiembro2 = (e: ChangeEvent<HTMLInputElement>) => {
+		setMiembro2({miembro: e.target.value});
+	};
 
-	  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+	const onSubmit = (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
-	  };
-	
+	};
+
 	return (
 		<>
 			<h1>Actividad 1 de refuerzo</h1>
 			<hr />
 			<form onSubmit={onSubmit}>
-
-			<label htmlFor="miembro1">Miembro grupo 1:</label>
-			<input id="miembro1" type="text" onChange={onChangeMiembro1} />
-			<label htmlFor="miembro2">Miembro grupo 2:</label>
-			<input id="miembro2" type="text" onChange={onChangeMiembro1} />
-			<button
-          className="btn btn-success"
-          type="submit"
-          disabled={miembro1.trim() === "" && miembro2.trim() === ""}
-        >
-          Añadir
-        </button>
-
+				<label htmlFor="miembro1">Miembro grupo 1:</label>
+				<input id="miembro1" type="text" onChange={onChangeMiembro1} />
+				<label htmlFor="miembro2">Miembro grupo 2:</label>
+				<input id="miembro2" type="text" onChange={onChangeMiembro1} />
+				<button
+					className="btn btn-success"
+					type="submit"
+					disabled={miembro1.miembro.trim() === "" && miembro2.miembro.trim() === ""}
+				>
+					Añadir
+				</button>
 			</form>
 		</>
 	);
