4782120247b9746792fa0792de1bab12c5a49819
Quico Saval <quicosv@gmail.com>
2023-06-07 19:30:05 +0200

    Plantea las funciones auxiliares para la actividad 7

---
+++ otras/actividad7/index.ts
@@ -0,0 +1,50 @@
+// Aquí van las funciones auxiliares
+
+const createArray = (length: number): number[] => {
+	let array: number[] = [];
+	for (let i = 0; i < length; i++) {
+		const newElement: number = Math.floor(Math.random() * 100) + 1;
+		array.push(newElement);
+	};
+	return array;
+};
+
+const showArrayWhile = (array: number[]): void => {
+	console.log("Mostrando elementos con el antiquísimo bucle while:");
+	let counter = 0;
+	while (counter < array.length) {
+		console.log(array[counter]);
+	};
+};
+
+const showArrayFor = (array: number[]): void => {
+	console.log("Mostrando elementos con el moderno bucle for:");
+	for (const element of array) {
+		console.log(element);
+	};
+};
+
+const showArrayForEach = (array: number[]): void => {
+	console.log("Mostrando elementos con el modernísimo forEach:");
+	array.forEach((x) => console.log(x));
+};
+
+const calculateAverage = (array: number[]): number => {
+	/* 
+	reduce permite calcular la suma de los elementos del array.
+
+	El primer argumento es la variable que irá recogiendo la suma.
+
+	El segundo argumento es el elemento del array por el que va la iteración.
+
+	Después de la flecha se pone la fórmula que tiene que hacer (en este caso la suma), una coma y el valor inicial de la variable que recoge la suma (en este caso 0).
+	*/
+	let total = array.reduce((subTotal, value) => subTotal + value, 0);
+	return total / array.length;
+};
+
+// aquí está la función principal, que debe manejar todo el programa
+
+const main = (): void => {};
+
+main();
