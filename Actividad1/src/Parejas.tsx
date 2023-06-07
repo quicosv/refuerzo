f72d8cecac635becd578d2286e10b9b7f172809e
piscinagua <correo_javi@hotmail.com>
2023-06-07 17:34:42 +0200

    Crea función para generar números aleatorios con el fin de asignar a cada elemento del array grupo1, otro elemento del array grupo2. Falta pintarlo en la página.

--- Actividad1/src/Parejas.tsx
+++ Actividad1/src/Parejas.tsx
@@ -4,8 +4,12 @@ import { IMiembro } from "./interfaces/miembro.interface";
 export const Parejas = () => {
 	const [miembro1, setMiembro1] = useState<IMiembro>({ miembro: "" });
 	const [miembro2, setMiembro2] = useState<IMiembro>({ miembro: "" });
+	const [emparejaos, setEmparejaos] = useState<boolean>(false);
+	// Nos declaramos los arrays con ambos grupos
 	const grupo1: IMiembro[] = [];
 	const grupo2: IMiembro[] = [];
+	// Aquí creamos array de números donde almacenamos índices aleatorios para luego mostrar las parejas
+	const posicionesDeParejas: number[] = [];
 
 	const onChangeMiembro1 = (e: ChangeEvent<HTMLInputElement>) => {
 		setMiembro1({ miembro: e.target.value });
@@ -21,9 +25,42 @@ export const Parejas = () => {
 		grupo2.push(miembro2);
 	};
 
-const formarParejas = () => {
+	const generadorNumerosAleatoriosUnicos = (
+		cuantosDistintos: number,
+		min: number,
+		max: number
+	): number[] => {
+		if (cuantosDistintos > max - min + 1) {
+			throw new Error(
+				"No se pueden generar tantos números distintos dentro del rango especificado."
+			);
+		}
 
-};
+		const numeros: number[] = [];
+
+		while (numeros.length < cuantosDistintos) {
+			const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
+			if (!numeros.includes(randomNumber)) {
+				numeros.push(randomNumber);
+			}
+		}
+
+		return numeros;
+	};
+
+	const formarParejas = () => {
+		// generamos, por cada elemento del primer array grupo1, el índice que le correspondería
+		// de la pareja del grupo2, ya que ambos arrays serán iguales
+		// esto obtendrá un índice que apuntará a una pareja única y aleatoria de grupo2 para cada uno.
+		const posicionesDeParejas = generadorNumerosAleatoriosUnicos(
+			grupo1.length,
+			0,
+			grupo1.length - 1
+		);
+		// ponemos el useState de emparejaos a true para saber cuándo mostrar los resultados
+		setEmparejaos(true);
+	};
 
 	return (
 		<>
@@ -33,12 +70,13 @@ const formarParejas = () => {
 				<label htmlFor="miembro1">Miembro grupo 1:</label>
 				<input id="miembro1" type="text" onChange={onChangeMiembro1} required />
 				<label htmlFor="miembro2">Miembro grupo 2:</label>
-				<input id="miembro2" type="text" onChange={onChangeMiembro1} required />
+				<input id="miembro2" type="text" onChange={onChangeMiembro2} required />
 				<button className="btn btn-success" type="submit">
 					Añadir
 				</button>
 			</form>
 			<button onClick={formarParejas}>Formar parejas</button>
+			{emparejaos && <h2>Resultados de parejas</h2>}
 		</>
 	);
 };
