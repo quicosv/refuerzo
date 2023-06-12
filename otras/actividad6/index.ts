b98bc1256b04d80f894785bc994e3cb7dccd714d
Quico Saval <quicosv@gmail.com>
2023-06-12 17:18:40 +0200

    Arregla el medio rombo

--- otras/actividad6/index.ts
+++ otras/actividad6/index.ts
@@ -53,7 +53,16 @@ const triangle = (): void => {
 
 const halfRhombus = (): void => {
 	const canvas: HTMLDivElement = document.querySelector('#medioRombo')!;
-	triangle();
+	for (let rowCounter = 1; rowCounter <= 10; rowCounter++) {
+		const row = document.createElement('div');
+		row.classList.add('row');
+		for (let columnCounter = 1; columnCounter <= rowCounter; columnCounter++) {
+			const column = document.createElement('span');
+			column.classList.add('dot');
+			row.appendChild(column);
+		}
+		canvas.appendChild(row);
+	}
 	for (let rowCounter = 9; rowCounter >= 1; rowCounter--) {
 		const row = document.createElement('div');
 		row.classList.add('row');
